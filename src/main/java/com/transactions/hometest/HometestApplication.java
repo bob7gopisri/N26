2e11f277a3de46b858a515e1afc9062050f7315c
Gopi Srinath <bob7gopisri@gmail.com>
2017-09-24 01:33:18 +0530

    Cleanup

--- src/main/java/com/transactions/hometest/HometestApplication.java
+++ src/main/java/com/transactions/hometest/HometestApplication.java
@@ -3,13 +3,6 @@ package com.transactions.hometest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-//@Configuration
-//@EnableAutoConfiguration
-//@ComponentScan({"controller", "service"})
 @SpringBootApplication(scanBasePackages={"com.transactions.hometest"})
 public class HometestApplication {
 	public static void main(String[] args) {
